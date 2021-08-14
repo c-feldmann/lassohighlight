35ef28c47cd15430ec0590f6884c9523675249f4
cfeldmann <ueb.garnix@googlemail.com>
2021-08-14 17:16:09 +0200

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='lassohighlight',
+    version='0.1',
+    author='Christian Feldmann',
+    license="BSD",
+    packages=['lassohighlight', ],
+    author_email='cfeldmann@bit.uni-bonn.de',
+    description='Lasso highlighting for RDKit',
+    install_requires=['numpy', ]
+)
